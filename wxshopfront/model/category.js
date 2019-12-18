49bfba225ef2bd61b7e46477c512e47f33f511e0
sunweiguo <317758022@qq.com>
2019-12-18 19:54:22 +0800

    提交人：fossi 提交内容：分类宫格 1、利用lin-ui宫格组件，进行分类的宫格展示

---
+++ wxshopfront/model/category.js
@@ -0,0 +1,13 @@
+import {Http} from "../utils/http";
+
+class Category{
+    static async getGridCategory() {
+        return await Http.request({
+            url: `category/grid/all`
+        })
+    }
+}
+
+export {
+    Category
+}
--- wxshopfront/pages/home/home.js
+++ wxshopfront/pages/home/home.js
@@ -2,6 +2,7 @@
 
 import {Theme} from "../../model/theme";
 import {Banner} from "../../model/banner";
+import {Category} from "../../model/category";
 
 Page({
 
@@ -10,7 +11,8 @@ Page({
    */
   data: {
     themeA:null,
-    bannerB:null
+    bannerB:null,
+    grid:[]
   },
 
   /**
@@ -24,9 +26,11 @@ Page({
   async initAllData() {
     const themeA = await Theme.getHomeLocationA()
     const bannerB = await Banner.getHomeLocationB()
+    const grid = await Category.getGridCategory()
     this.setData({
       themeA: themeA[0],
-      bannerB: bannerB
+      bannerB: bannerB,
+      grid:grid
     })
   },
 
